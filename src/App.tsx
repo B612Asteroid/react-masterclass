5457ada9ad0883cced67801f89f47cd1368971db
Ralph <pok12369@naver.com>
2023-06-06 09:10:50 +0900

    [INIT] INIT

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+import Router from "./Router";
+
+const Container = styled.div`
+    background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor}
+`;
+
+function App() {
+
+  return (
+      <Router />
+  );
+}
+
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
+
+function Router() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/:coinId">
+                <Coin />
+            </Route>
+            <Route path="/coins">
+                <Coins />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Router;
